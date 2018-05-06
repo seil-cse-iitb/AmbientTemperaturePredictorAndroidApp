2c216af2ec46dbeb2cc2ed9ea2933cd10e6bb5ab
Sapan <sapantanted99@gmail.com>
2018-05-06 08:41:46 +0530

    Getting Battery Temperature reading

--- app/src/main/java/com/android/sapantanted/ambienttemperaturepredictor/MainActivity.java
+++ app/src/main/java/com/android/sapantanted/ambienttemperaturepredictor/MainActivity.java
@@ -1,13 +1,38 @@
 package com.android.sapantanted.ambienttemperaturepredictor;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.BatteryManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView tvAmbientTemperature;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public final void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvAmbientTemperature = findViewById(R.id.tvAmbientTemperature);
+        IntentFilter intentfilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+        MainActivity.this.registerReceiver(broadcastreceiver, intentfilter);
     }
+
+    private BroadcastReceiver broadcastreceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            double temperature = (double)intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, 0) / 10;
+            tvAmbientTemperature.setText("Battery Temperature: " + temperature + " " + (char) 0x00B0 + "C");
+        }
+    };
 }
+
