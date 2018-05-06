b5ce8e726820f743e6033a4bb55d8b64f32f8c2f
Sapan <sapantanted99@gmail.com>
2018-05-06 06:13:29 +0530

    initial commit

---
+++ app/src/main/java/com/android/sapantanted/ambienttemperaturepredictor/MainActivity.java
@@ -0,0 +1,13 @@
+package com.android.sapantanted.ambienttemperaturepredictor;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
